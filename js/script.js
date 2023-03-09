1937b1c85e70a1d62e1c0b7932f5ac3b91b47acc
FranPer23 <francescaprt@yahoo.com>
2023-03-09 14:52:14 +0100

    aggiunta campi per inserimento dati

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const sumbitBtn = document.getElementById("submit-btn");
+sumbitBtn.addEventListener("click", function() {
+    const result = document.getElementById("result");
+    
+
+    // input del consumatore
+    const userNameInput = document.getElementById("user-name");
+    const userName = userNameInput.value;
+    console.log (userName)
+
+    const userKmInput = document.getElementById("user-km");
+    const userKm = userKmInput.value;
+    console.log (userKm)
+
+    const userAgeInput = document.getElementById("user-age");
+    const userAge = userAgeInput.value;
+    console.log (userAge)
+    
+
+
+// calcolo del prezzo del biglietto
+const kmPrice = 0.21;
+const  ticketPriceFull = kmPrice * userKm
+
+let ticketPrice = ticketPriceFull;
+
+if (userAge=minorenne) {
+    ticketPrice = ticketPriceFull - (ticketPriceFull * 20 / 100);
+
+} else if (userAge=maggiorenne) {
+    
+    ticketPrice = ticketPriceFull - (ticketPriceFull * 40 / 100);
+}
+
+// risultato con dati consumatore e prezzo
+result.innerHTML = `NOME PASSEGGERO ${userName}, devi percorrere km ${userKm} il prezzo è ${ticketPrice.toFixed(2)}`;
+
+})
+
